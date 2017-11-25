32e38553ff5ab54e9c461a722c9598093336cd50
simarc86 <simarc86@gmail.com>
2017-11-25 12:33:13 +0100

    Initial Commit

---
+++ FastLove/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FastLove
+//
+//  Created by Marc Tamarit Romero on 25/11/17.
+//  Copyright © 2017 Marc Tamarit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
